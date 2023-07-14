ce0a0ad5ce76bfe3a80a5f7cab0c3bd7d8f8397b
YujiKono-Nagoya <yuji.kono.19@gmail.com>
2023-07-15 08:28:51 +0900

    2023/07/14

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_serch/firebase_options.dart';
+import 'package:flutter_serch/pages/home.dart';
+
+main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'books',
+      theme: ThemeData(
+        primaryColor: Colors.green,
+      ),
+      home: Home(),
+    );
+  }
+}
